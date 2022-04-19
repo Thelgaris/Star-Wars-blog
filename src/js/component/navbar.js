9201ffc9190fe3455ed590572a345931f664b2f9
Thelgaris <alex.gabarron84@gmail.com>
2022-04-19 11:37:24 +0000

    withFavbuttonasdropdown

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,22 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1"><FontAwesomeIcon icon="fa-solid fa-jedi" /></span>
+				<span className="navbar-brand mb-0 h1"></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
+				<div class="dropdown">
+  						<button class="btn btn-prymary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   						Favourites
+  						</button>
+  						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    					<li><a class="dropdown-item" href="#">Action</a></li>
+    					<li><a class="dropdown-item" href="#">Another action</a></li>
+    					<li><a class="dropdown-item" href="#">Something else here</a></li>
+  						</ul>
+				</div>
 			</div>
 		</nav>
 	);
